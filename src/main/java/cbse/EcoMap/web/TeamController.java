4723756d35b7a7b7a3b29facc910df3cca130ad4
adina <adina.liyana@gmail.com>
2023-12-04 17:11:20 +0800

    comment initializer and added vite with react

--- src/main/java/cbse/EcoMap/web/TeamController.java
+++ src/main/java/cbse/EcoMap/web/TeamController.java
@@ -34,30 +34,30 @@ public class TeamController {
 	    }
 
 	    @GetMapping("/team/{id}")
-	    ResponseEntity<?> getGroup(@PathVariable Long id) {
+	    ResponseEntity<?> getTeam(@PathVariable Long id) {
 	        Optional<Team> team = teamRepository.findById(id);
 	        return team.map(response -> ResponseEntity.ok().body(response))
 	                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
 	    }
 
-	    @PostMapping("/group")
-	    ResponseEntity<Team> createGroup(@Valid @RequestBody Team group) throws URISyntaxException {
-	        log.info("Request to create group: {}", group);
-	        Team result = teamRepository.save(group);
-	        return ResponseEntity.created(new URI("/api/group/" + result.getId()))
+	    @PostMapping("/team")
+	    ResponseEntity<Team> createTeam(@Valid @RequestBody Team team) throws URISyntaxException {
+	        log.info("Request to create team: {}", team);
+	        Team result = teamRepository.save(team);
+	        return ResponseEntity.created(new URI("/api/team/" + result.getId()))
 	                .body(result);
 	    }
 
-	    @PutMapping("/group/{id}")
-	    ResponseEntity<Team> updateGroup(@Valid @RequestBody Team team) {
-	        log.info("Request to update group: {}", team);
+	    @PutMapping("/team/{id}")
+	    ResponseEntity<Team> updateTeam(@Valid @RequestBody Team team) {
+	        log.info("Request to update team: {}", team);
 	        Team result = teamRepository.save(team);
 	        return ResponseEntity.ok().body(result);
 	    }
 	    
-	    @DeleteMapping("/group/{id}")
-	    public ResponseEntity<?> deleteGroup(@PathVariable Long id) {
-	        log.info("Request to delete group: {}", id);
+	    @DeleteMapping("/team/{id}")
+	    public ResponseEntity<?> deleteTeam(@PathVariable Long id) {
+	        log.info("Request to delete team: {}", id);
 	        teamRepository.deleteById(id);
 	        return ResponseEntity.ok().build();
 	    }
