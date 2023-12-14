8bf9a229a9e1107bea2f74c75dde545fff3bc04b
adina <adina.liyana@gmail.com>
2023-12-15 01:51:36 +0800

    multiple medias for each litter card

--- ecomap-react/src/components/Pages/Litter/LitterList.jsx
+++ ecomap-react/src/components/Pages/Litter/LitterList.jsx
@@ -1,88 +1,137 @@
 import React, { useState } from "react";
 import { Grid, Card, CardCover, CardContent, Typography, Modal, IconButton } from '@mui/joy';
 import CloseIcon from '@mui/icons-material/Close';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { Button } from 'react-bootstrap';
 
-export default function LitterList({ mediaList }) {
-    const [hoveredIndex, setHoveredIndex] = useState(null);
+export default function LitterList({ litterList }) {
     const [openModal, setOpenModal] = useState(false);
     const [modalContent, setModalContent] = useState(null);
+    const [currentIndex, setCurrentIndex] = useState(0);
 
-    const handleCardClick = (media) => {
-        setModalContent(media);
+    const handleCardClick = (litter, index) => {
+        setModalContent(litter);
+        setCurrentIndex(index);
         setOpenModal(true);
     };
 
     const handleCloseModal = () => {
         setModalContent(null);
+        setCurrentIndex(0);
         setOpenModal(false);
     };
 
     return (
         <div>
-            <Grid
-                container
-                spacing={2}
-            >
-                {mediaList.map((media, index) => (
+            <Grid container spacing={2}>
+                {litterList.map((litter, index) => (
                     <Grid
                         key={index}
                         item
                         xs={12}
                         sm={6}
                         md={4}
-                        onMouseEnter={() => setHoveredIndex(index)}
-                        onMouseLeave={() => setHoveredIndex(null)}
                     >
-                        <Card sx={{ position: 'relative', width: '100%', height: '300px', borderRadius: '5px' }}>
+                        <Card
+                            sx={{
+                                position: 'relative',
+                                width: '100%',
+                                height: '300px',
+                                borderRadius: '5px',
+                                cursor: 'default',
+                            }}
+                        >
                             <CardCover>
-                                {media.type === 'image' && (
-                                    <img
-                                        src={media.src}
-                                        loading="lazy"
-                                        alt=""
-                                        style={{ width: '100%', height: '100%', objectFit: 'cover', borderRadius: '5px' }}
-                                    />
-                                )}
-                                {media.type === 'video' && (
-                                    <video
-                                        ref={(videoRef) => {
-                                            if (videoRef) {
-                                                if (hoveredIndex === index) {
-                                                    videoRef.play();
-                                                } else {
-                                                    videoRef.pause();
-                                                    videoRef.currentTime = 0;
-                                                }
-                                            }
+                                {litter.medias.map((media, mediaIndex) => (
+                                    <div
+                                        key={mediaIndex}
+                                        style={{
+                                            display: mediaIndex === currentIndex ? 'block' : 'none',
                                         }}
-                                        loop
-                                        muted
-                                        style={{ width: '100%', height: '100%', objectFit: 'cover', borderRadius: '5px' }}
                                     >
-                                        <source src={media.src} type="video/mp4" />
-                                    </video>
-                                )}
+                                        {media.type === 'image' && (
+                                            <img
+                                                src={media.src}
+                                                loading="lazy"
+                                                alt=""
+                                                style={{
+                                                    width: '100%',
+                                                    height: '100%',
+                                                    objectFit: 'cover',
+                                                    borderRadius: '5px',
+                                                }}
+                                            />
+                                        )}
+                                        {media.type === 'video' && (
+                                            <video
+                                                ref={(videoRef) => {
+                                                    if (videoRef) {
+                                                        if (mediaIndex === currentIndex) {
+                                                            videoRef.play();
+                                                        } else {
+                                                            videoRef.pause();
+                                                            videoRef.currentTime = 0;
+                                                        }
+                                                    }
+                                                }}
+                                                loop
+                                                muted
+                                                style={{
+                                                    width: '100%',
+                                                    height: '100%',
+                                                    objectFit: 'cover',
+                                                    borderRadius: '5px',
+                                                }}
+                                            >
+                                                <source src={media.src} type="video/mp4" />
+                                            </video>
+                                        )}
+                                    </div>
+                                ))}
                             </CardCover>
                             <CardContent
                                 sx={{
                                     position: 'absolute',
                                     bottom: 0,
                                     left: 0,
+                                    right: 0,
                                     padding: 2,
-                                    width: '100%',
                                     display: 'flex',
-                                    flexDirection: 'row', 
-                                    justifyContent: 'space-between'
-                                }}>
-                                <Typography
-                                    level="body-lg"
-                                    fontWeight="lg"
-                                    textColor="#fff"
+                                    flexDirection: 'column',
+                                    justifyContent: 'space-between',
+                                    alignItems: 'right',
+                                    width: "100%",
+                                    height: "100%",
+                                }}
+                            >
+                                <p
+                                    style={{color: "#fff"}}
                                 >
-                                    {media.type === 'image' ? 'Image' : 'Video'}
-                                </Typography>
-                                <Button variant='light' onClick={() => handleCardClick(media)}>View</Button>
+                                    {`1/${litter.medias.length}`}
+                                </p>
+                                <div
+                                    style={{
+                                        width: '100%',
+                                        display:"flex", 
+                                        justifyContent:"space-between",
+                                        bottom: 0
+                                    }}
+                                >
+                                    <Typography
+                                        level="body-lg"
+                                        fontWeight="lg"
+                                        textColor="#fff"
+                                    >
+                                        {litter.title}
+                                    </Typography>
+                                    <Button
+                                        variant='light'
+                                        onClick={() => handleCardClick(litter, index)}
+                                    >
+                                        View
+                                    </Button>
+                                </div>
                             </CardContent>
                         </Card>
                     </Grid>
@@ -104,21 +153,36 @@ export default function LitterList({ mediaList }) {
                 <div>
                     {modalContent && (
                         <>
-                            {modalContent.type === 'image' && (
-                                <img
-                                    src={modalContent.src}
-                                    alt=""
-                                    style={{ maxWidth: '100%', maxHeight: '550px' }}
-                                />
-                            )}
-                            {modalContent.type === 'video' && (
-                                <video
-                                    controls
-                                    style={{ maxWidth: '100%', maxHeight: '550px' }}
+                            {modalContent.medias.map((media, mediaIndex) => (
+                                <div
+                                    key={mediaIndex}
+                                    style={{
+                                        display: mediaIndex === currentIndex ? 'block' : 'none',
+                                    }}
                                 >
-                                    <source src={modalContent.src} type="video/mp4" />
-                                </video>
-                            )}
+                                    {media.type === 'image' && (
+                                        <img
+                                            src={media.src}
+                                            alt=""
+                                            style={{
+                                                maxWidth: '100%',
+                                                maxHeight: '550px',
+                                            }}
+                                        />
+                                    )}
+                                    {media.type === 'video' && (
+                                        <video
+                                            controls
+                                            style={{
+                                                maxWidth: '100%',
+                                                maxHeight: '550px',
+                                            }}
+                                        >
+                                            <source src={media.src} type="video/mp4" />
+                                        </video>
+                                    )}
+                                </div>
+                            ))}
                             <IconButton
                                 onClick={handleCloseModal}
                                 sx={{
@@ -131,7 +195,7 @@ export default function LitterList({ mediaList }) {
                                 <CloseIcon />
                             </IconButton>
                             <Typography variant="h5" id="modal-title" sx={{ mt: 2, color: 'white' }}>
-                                {modalContent.caption}
+                                {modalContent.title}
                             </Typography>
                         </>
                     )}
