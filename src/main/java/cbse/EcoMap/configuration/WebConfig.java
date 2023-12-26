2b8a4ca5c43fd00ab138adf4281023294eba183f
sarahatiqah <sarahatiqahamram@gmail.com>
2023-12-27 01:42:24 +0800

    Allow CORS requests from localhost:4200

--- src/main/java/cbse/EcoMap/configuration/WebConfig.java
+++ src/main/java/cbse/EcoMap/configuration/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") 
-                .allowedOrigins("http://127.0.0.1:4200")
+                .allowedOrigins("http://127.0.0.1:4200", "http://localhost:4200")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
 }
